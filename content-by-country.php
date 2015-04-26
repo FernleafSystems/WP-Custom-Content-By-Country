cf96dfcd69dafbdc4b1df91529a3022ba51f4250
Paul <paul@icontrolwp.com>
2015-04-26 20:17:47 +0100

    update db version constant

--- content-by-country.php
+++ content-by-country.php
@@ -32,7 +32,7 @@ Author URI: http://icwp.io/home
 require_once( dirname(__FILE__).'/src/icwp-base.php' );
 class ICWP_CustomContentByCountry_Plugin extends ICWP_CCBC_Wordpress_Plugin_V1 {
 
-	const Ip2NationDbVersion = '20140816D';
+	const Ip2NationDbVersion = '20150218A';
 
 	const Ip2NationDbVersionKey = 'ip2nation_version';
 
